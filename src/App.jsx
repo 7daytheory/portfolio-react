953b14541d606f8028572a50d1103c301058719f
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-03 18:00:12 -0400

    Create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>App Home Page</h1>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            
+        </div>
+      </div>
+    
+    </BrowserRouter>
   )
 }
 
