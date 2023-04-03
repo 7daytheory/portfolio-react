942b5c126b182015d2ea92648e05de30d4245c5d
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-03 18:10:14 -0400

    Setup all the components and add to DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,16 @@ function App() {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            
+            <Navbar />
+            <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
         </div>
       </div>
     
