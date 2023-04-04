e2c99f3fab8dea38685fdd1388dfb298cf3bf0e4
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-04 17:50:08 -0400

    Add logo that brings user to top of page when clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,24 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
+  const [first, setFirst] = useState("");
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center fixed top-0 z-20 bg-primary}`}>
+        <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+            <Link
+              to="/"
+              className="flex items-center gap-2"
+              onClick={() => {
+                setActive("");
+                window.scrollTo(0,0); //Scroll to top of page
+              }}
+              >
+                <img src={logo} alt="logo" className="w-2 h-2 object-contain" />
+            </Link>
+        </div>
+    </nav>
   )
 }
 
