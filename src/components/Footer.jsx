d11d5494c0fd1277f2b71323cfa357e805a7dd06
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-11 17:10:26 -0400

    Added HoF and animation to footer, edited contact styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+import { SectionWrapper } from '../hoc';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <footer
-    className="flex justify-center mt-4 mb-4">
-      <p>Matthew Lowe Portfolio</p>
-    </footer>
+    <>
+    <motion.div 
+    variants={fadeIn("right", "tween", 0.2, 2)}>
+      <footer
+      className="flex justify-center mb-0"
+      style={{marginTop: '-75px'}}>
+        <p>Matthew Lowe Portfolio {year}</p>
+      </footer>
+    </motion.div>
+    </>
   )
 }
 
-export default Footer;
+export default SectionWrapper(Footer, 'footer');
