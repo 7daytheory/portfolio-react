88bd0ee9adae0a8e4a160eeb724f2e8322dec57c
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-11 18:47:31 -0400

    Changed footer background to purple used in header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
-import { SectionWrapper } from '../hoc';
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <>
     <motion.div 
-    variants={fadeIn("right", "tween", 0.2, 2)}>
+    variants={fadeIn("right", "tween", 0.2, 2)}
+>
       <footer
       className="flex justify-center mb-0"
-      style={{marginTop: '-75px'}}>
-        <p>Matthew Lowe Portfolio {year}</p>
+      style={{position: 'absolute', width: '100%', height: '75px', background: '#915eff', color: '#FFF', fontWeight: 'bold', letterSpacing: '1px'}}
+      >
+        <p style={{marginTop: '25px'}}>Matthew Lowe Portfolio {year}</p>
       </footer>
     </motion.div>
     </>
   )
 }
 
-export default SectionWrapper(Footer, 'footer');
+export default Footer;
