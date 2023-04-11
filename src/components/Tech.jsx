a96ea99abf2f37503e1eb217aae56b016f6ef47e
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-11 19:47:38 -0400

    Added BallCanvas to create 3D icons for tech stack

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,12 +6,12 @@ const Tech = () => {
     return (
         <div className="flex flex-row flex-wrap justify-center gap-10">
             {technologies.map((technology) => (
-                <div className="w-28 h-28" key={technologyLogo.name}>
-                    <BallCanvas icon={techology.icon} />
+                <div className="w-28 h-28" key={technology.name}>
+                    <BallCanvas icon={technology.icon} />
                 </div>
             ))}
         </div>
     )
 }
 
-export default Tech;
+export default SectionWrapper(Tech, 'tech');
